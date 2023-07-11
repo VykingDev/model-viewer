69375a5ca53eee034426f561904c2ff59a6e0832
stevebritton <steve@vyking.io>
2023-07-11 15:52:45 +0100

    Bumped version to 1.4a, ready for next release

--- packages/model-viewer/src/features/vyking.ts
+++ packages/model-viewer/src/features/vyking.ts
@@ -46,7 +46,7 @@ export const VykingMixin = <T extends Constructor<ModelViewerElementBase>>(
             return this[$vykingSrc]
         }
 
-        #VykingMixinVersion = "3.1.1-1.3"
+        #VykingMixinVersion = "3.1.1-1.4a"
         #internetLoggingProperties = {
             isSuspended: false,
             loggingEnabled: true,
